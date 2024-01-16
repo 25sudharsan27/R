c1fd6ef95db9e049db3f62611589e270958e4b16
25sudharsan27 <sudharsan@lpu.in>
2024-01-16 11:22:00 +0530

    First programm in our class

---
+++ 1firstprogram.r
@@ -0,0 +1,34 @@
+v <- TRUE
+print(class(v))
+name <- "first program in R"
+print(class(name))
+print(name)
+
+# Integer
+m <- 2L
+
+# numeric
+x <- 2
+
+# numeric
+n <- m*x
+
+# numeric
+l <- 9.4
+
+# complex
+u <- 20 + 50i
+s <- 10 + 100i
+
+print(u+s)
+ 
+print(class(n))
+print(class(u))
+
+# Getting input
+# name1 <- readline('What is your name : ')
+# print("Welcome")
+# print(name1)
+
+
+list <- 23L
