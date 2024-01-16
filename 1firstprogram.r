3a360d87d3ac873304d536e2e52a7cdbcaeb83fa
25sudharsan27 <sudharsan@lpu.in>
2024-01-16 11:55:32 +0530

    first problem solved in the r programming

--- 1firstprogram.r
+++ 1firstprogram.r
@@ -1,38 +1,66 @@
-v <- TRUE
-print(class(v))
-name <- "first program in R"
-print(class(name))
-print(name)
+# # v <- TRUE
+# # print(class(v))
+# # name <- "first program in R"
+# # print(class(name))
+# # print(name)
 
-# Integer
-m <- 2L
+# # # Integer
+# # m <- 2L
 
-# numeric
-x <- 2
+# # # numeric
+# # x <- 2
 
-# numeric
-n <- m*x
+# # # numeric
+# # n <- m*x
 
-# numeric
-l <- 9.4
+# # # numeric
+# # l <- 9.4
 
-# complex
-u <- 20 + 50i
-s <- 10 + 100i
+# # # complex
+# # u <- 20 + 50i
+# # s <- 10 + 100i
 
-print(u+s)
+# # print(u+s)
  
-print(class(n))
-print(class(u))
+# # print(class(n))
+# # print(class(u))
 
-# Getting input
-# name1 <- readline('What is your name : ')
-# print("Welcome")
-# print(name1)
+# # # Getting input
+# # # name1 <- readline('What is your name : ')
+# # # print("Welcome")
+# # # print(name1)
 
 
-list <- 23L
+# # list <- 23L
 
-var_name2. =1
+# # var_name2. =1
 
-print(var_name2.)
+# # #Dot can be used first but it is not followed by the number, if followed by number then it is invalid
+# # .var_name =3
+# # print(.var_name)
+
+# # print(var_name2.)
+
+# # cat("This is printed by using cat function", name, '\n')
+# # print("hi")
+
+# # x <- 4
+# # b=5
+# # x+b -> c
+
+# # #cat is used to concatenate the two strings
+# # cat("This is the sum",c)
+# # cat("\n")
+# # y <- 7*1+10-1/2
+# # cat(y,'\n')
+
+# # x <- 5
+# # print(ls())
+# # rm(x)
+
+# rm(b)
+# rm(c)
+# rm(l)
+# rm(list)
+# print(ls())
+
---
+++ 2problem.r
@@ -0,0 +1,17 @@
+a <- readline("Enter first number: ")
+b <- readline("Enter second number: ")
+c <- readline("Enter third number: ")
+
+print("----------------------------------------")
+
+cat("         Sum of the numbers is :")
+cat(as.integer(a) + as.integer(b)+as.integer(c))
+cat("\n")
+
+print("-----------------------------------------")
+
+cat ("        Product of numbrs is :")
+cat(as.integer(a) * as.integer(b)*as.integer(c))
+cat("\n")
+
+print("----------------------------------------")
