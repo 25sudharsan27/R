2b2b6bf0307041d324a085f7ad76549ecb3b4b65
25sudharsan27 <sudharsan@lpu.in>
2024-01-18 11:15:50 +0530

    lectture 3

---
+++ a1operations.r
@@ -0,0 +1,14 @@
+# (+) ( - ) ( * ) ( / )  ( ** or ^ ) 
+
+a <- 10
+b <- 4
+
+print(a*b)
+print(a-b)
+print(a+b)
+print(a/b)
+print(a**b)
+print(a^b)
+print(a%%b)
+# here the operation is for quotient
+print(a%/%b) 
---
+++ a2vectors.r
@@ -0,0 +1,22 @@
+a <- c(10,4,3)
+b <- c(3,1,0)
+
+#Addition
+print(a+b)
+#Subtraction
+print(a-b)
+#Multiplication
+print(a*b)
+#Division
+print(a/b)
+#Modulus
+print(a%%b)
+# Quotient
+print(a%/%b)
+# power
+print(a^b)
+
+print(ls())
+
+rm(a)
+print(ls())
